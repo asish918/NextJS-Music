ec766e89175b37c2c6bcc03e5bcdb81d195c4499
Asish Mahapatra <asishmahapatra918@gmail.com>
2023-12-15 22:56:08 +0530

    Styled the HomePage with various components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Figtree } from 'next/font/google'
 import './globals.css'
+import Sidebar from '@/components/Sidebar'
 
 const inter = Figtree({ subsets: ['latin'] })
 
@@ -9,14 +10,22 @@ export const metadata: Metadata = {
   description: 'Sooth your ears!!',
 }
 
+export const revalidate = 0;
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const userSongs = await getSongsByUserId();
+
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Sidebar songs={userSongs}>
+          {children}
+        </Sidebar>
+      </body>
     </html>
   )
 }
---
+++ types.ts
@@ -0,0 +1,62 @@
+export interface Song {
+  id: string;
+  user_id: string;
+  author: string;
+  title: string;
+  song_path: string;
+  image_path: string;
+}
+
+export interface Product {
+  id: string;
+  active?: boolean;
+  name?: string;
+  description?: string;
+  image?: string;
+}
+
+export interface Price {
+  id: string;
+  product_id?: string;
+  active?: boolean;
+  description?: string;
+  unit_amount?: number;
+  currency?: string;
+  interval_count?: number;
+  trial_period_days?: number | null;
+  products?: Product;
+}
+
+export interface Customer {
+  id: string;
+  stripe_customer_id?: string;
+}
+
+export interface UserDetails {
+  id: string;
+  first_name: string;
+  last_name: string;
+  full_name?: string;
+  avatar_url?: string;
+}
+
+export interface ProductWithPrice extends Product {
+  prices?: Price[];
+}
+
+export interface Subscription {
+  id: string;
+  user_id: string;
+  price_id?: string;
+  quantity?: number;
+  cancel_at_period_end?: boolean;
+  created: string;
+  current_period_start: string;
+  current_period_end: string;
+  ended_at?: string;
+  cancel_at?: string;
+  canceled_at?: string;
+  trial_start?: string;
+  trial_end?: string;
+  prices?: Price;
+}
