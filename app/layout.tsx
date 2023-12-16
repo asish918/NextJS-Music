87f5a8bc3352cbd08077254be07894327d01f315
Asish Mahapatra <asishmahapatra918@gmail.com>
2023-12-16 11:35:51 +0530

    Auth Modal setup for done

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from 'next'
 import { Figtree } from 'next/font/google'
 import './globals.css'
 import Sidebar from '@/components/Sidebar'
+import SupabaseProvider from '@/providers/SupabaseProvider'
+import UserProvider from '@/providers/UserProvider'
+import ModalProvider from '@/providers/ModalProvider'
 
 const inter = Figtree({ subsets: ['latin'] })
 
@@ -18,13 +21,20 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   const userSongs = await getSongsByUserId();
+  const products = await getActiveProductsWithPrices();
 
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Sidebar songs={userSongs}>
-          {children}
-        </Sidebar>
+        <SupabaseProvider>
+          <UserProvider>
+            <ModalProvider products={products} />
+            <Sidebar songs={userSongs}>
+              {children}
+            </Sidebar>
+            <Player />
+          </UserProvider>
+        </SupabaseProvider>
       </body>
     </html>
   )
