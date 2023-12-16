4e01d393182a02d7316cb094294c227b561ef98f
Asish Mahapatra <asishmahapatra918@gmail.com>
2023-12-16 12:43:43 +0530

    Fixed Bugs

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["hpfyimzqorewoixkukjh.supabase.co"]
+        domains: ["bhqmsowulepyeffibhks.supabase.co"]
     }
 }
 
